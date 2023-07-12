2f5115610fdadf29b30939999567d3cf62e0c95e
Ashika G <ashi.gogineni@gmail.com>
2023-07-12 15:01:23 -0700

    Initial Commit

---
+++ stempiration/stempirationApp.swift
@@ -0,0 +1,17 @@
+//
+//  stempirationApp.swift
+//  stempiration
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct stempirationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
